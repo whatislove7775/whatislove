e2f21322ce7031c9e59a4b713909e65d5fa29ff8
vlad m. <vladm0617@gmail.com>
2026-05-01 22:56:35 +0300

    Create Breadcrumbs.tsx

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,32 @@
+'use client';
+import Link from 'next/link';
+
+interface Props {
+  path: { name: string; href?: string; icon?: string }[];
+}
+
+export default function Breadcrumbs({ path }: Props) {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', fontWeight: 'bold', fontSize: '14px', marginBottom: '30px' }}>
+      <div style={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+        <Link href="/">[{"<"}]</Link>
+        {path.map((item, index) => (
+          <span key={index} style={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+            {item.href ? (
+              <Link href={item.href} style={{ textDecoration: 'none', color: '#000' }}>
+                {item.icon} {item.name}
+              </Link>
+            ) : (
+              <span>{item.icon} {item.name}</span>
+            )}
+            {index < path.length - 1 && <span>/</span>}
+          </span>
+        ))}
+      </div>
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <Link href="/">[🏠]</Link>
+        <Link href="/">[x]</Link>
+      </div>
+    </div>
+  );
+}
