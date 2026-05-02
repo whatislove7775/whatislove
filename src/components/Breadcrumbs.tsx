396b7c4e79881f5b435c4a6343d7f98b0f9211b6
vlad m. <vladm0617@gmail.com>
2026-05-02 11:55:16 +0300

    Update Breadcrumbs.tsx

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -7,7 +7,16 @@ interface Props {
 
 export default function Breadcrumbs({ path }: Props) {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', fontWeight: 700, textTransform: 'uppercase', marginBottom: '40px' }}>
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'space-between', 
+      width: '100%', 
+      fontWeight: 700, 
+      textTransform: 'uppercase', 
+      marginBottom: '40px',
+      position: 'relative', // Вытаскиваем на передний план
+      zIndex: 50          // Защита от перекрытия невидимыми блоками
+    }}>
       <div style={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
         <Link href="/" style={{ textDecoration: 'none', color: '#000' }}>[&lt;]</Link>
         <Link href="/" style={{ textDecoration: 'none', color: '#000' }}>📁 WH4T!SLOV3</Link>
