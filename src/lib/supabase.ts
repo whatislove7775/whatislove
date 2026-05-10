dba661a1887feb32ff5426c528bc7645c5b705e8
vlad m. <vladm0617@gmail.com>
2026-05-10 11:51:38 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 1. Оставляем только эту версию для теста
 export const supabase = createClient(
   'https://vhdxdmgcjweqdpkgptcx.supabase.co', 
   'sb_publishable_3b0cwrS_3TohAbSvb3eGmg_Kp_zq7YN'
 );
-
-// ВСЁ, ЧТО НИЖЕ (строки 12-15), ПРОСТО УДАЛИ ИЛИ ЗАКОММЕНТИРУЙ
