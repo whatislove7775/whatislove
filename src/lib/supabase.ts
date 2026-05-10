2afbec0aa2c1c18caa3042be8d7c62330470c05f
vlad m. <vladm0617@gmail.com>
2026-05-10 11:35:33 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,12 @@ import { createClient } from '@supabase/supabase-js';
 console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL ? 'OK' : 'MISSING');
 console.log('KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? 'OK' : 'MISSING');
 
+// ВРЕМЕННО для теста
+export const supabase = createClient(
+  'https://vhdxdmgcjweqdpkgptcx.supabase.co', 
+  'sb_publishable_3b0cwrS_3TohAbSvb3eGmg_Kp_zq7YN'
+);
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
